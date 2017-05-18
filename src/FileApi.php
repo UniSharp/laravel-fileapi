1267f13475655737768cbc12dfd60f67f1b3a71e
FreedomKnight <fk@unisharp.com>
2017-05-18 13:31:38 +0800

    fix to 100 quality

--- src/FileApi.php
+++ src/FileApi.php
@@ -232,15 +232,15 @@ class FileApi
                             switch ($exif['Orientation']) {
                                 case 8:
                                     $img = imagerotate($img, 90, 0);
-                                    imagejpeg($img, $image_path);
+                                    imagejpeg($img, $image_path, 100);
                                     break;
                                 case 3:
                                     $img = imagerotate($img, 180, 0);
-                                    imagejpeg($img, $image_path);
+                                    imagejpeg($img, $image_path, 100);
                                     break;
                                 case 6:
                                     $img = imagerotate($img, -90, 0);
-                                    imagejpeg($img, $image_path);
+                                    imagejpeg($img, $image_path, 100);
                                     break;
                             }
                         }
